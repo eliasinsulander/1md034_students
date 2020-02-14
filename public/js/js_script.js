70bb160b85c55d2c2e6cb1d8b1838b39e287209c
Elias Insulander <eliasinsulander@student-227-234.eduroam.uu.se>
2020-02-14 12:20:26 +0100

    order info

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,26 +1,35 @@
+
 /*
-function menuItem(name, kcal, g, l, img) {
-    this.name = name;
-    this.kcal = kcal;
-    this.gluten = g;
-    this.lactose = l;
-    this.img = img;
-    this.info = function() {
-	return this.name + ' ' + this.kcal;
-    };
-}
+let button = document.getElementById("button");
+button.onclick = function() {
+  console.log("Button clicked!");
+  console.log(info());
+};
 
-let burger1 = new menuItem('Mäktig Burgare', '100 000 kCal', 'Mycket glutenberikat bröd', 'Laktosfri', 'https://external-preview.redd.it/XBxy_zicanPx_Ws7DsCM0bnB9YsUY3gtacJ57KObgI8.jpg?auto=webp&s=412fa5f9334773a6ead0b85dc4a82e2dfa01270a');
 
-let burger2 = new menuItem('Sjaskig Burgare', '??? kCal', 'Glutenfri', 'Ost på råttmjölk', 'https://s3-media0.fl.yelpcdn.com/bphoto/r3H7q9mu6hjiqPZTbq5cYA/o.jpg');
 
-let burger3 = new menuItem('Nyburgare', '1000 kCal', 'Innehåller gluten', 'Vegansk', 'https://www.garantskafferiet.se/filer/02-vara-produkter/vegetariskt/7340083468773_c1l1.png?preset=product-page-image" alt="vegan burger');
+function info(){
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let street = document.getElementById("street").value;
+  let number = document.getElementById("number").value;
+  let payment = document.getElementById("payment").value;
+  let gender = gen();
 
-let burger4 = new menuItem('Liten Burgare', '20 kCal', 'Glutenfylld', 'Laktosfylld', 'https://aht.seriouseats.com/images/20110407-every-burger-hand.jpg');
+  return [name, email, street, number, payment, gender];
+};
 
-let burger5 = new menuItem('Toast', '2000 kCal', 'Glutenfri', 'Laktosfri', 'https://food-images.files.bbci.co.uk/food/recipes/savoury_french_toast_44950_16x9.jpg');
-
-var menu = [burger1, burger2, burger3, burger4, burger5]
+function gen(){
+  if (document.getElementById("male").checked) {
+    return document.getElementById("male").value;
+  } else if (document.getElementById("female").checked){
+    return document.getElementById("female").value;
+  } else if (document.getElementById("nb").checked){
+    return document.getElementById("nb").value;
+  } else {
+    return document.getElementById("other").value;
+  }
+};
 */
 
 /*
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 
-  const vm = new Vue({
+  const vm1 = new Vue({
     el: '.wrapper',
     data: {
       //mäktig
@@ -37,6 +37,72 @@ window.onload = function(){
       tLac: food[4].lactose
     }
   });
+
+  const vm2 = new Vue({
+    el: '#form',
+    data: {
+      name: "",
+      email: "",
+      street: "",
+      number: "",
+      payment: "",
+      gender: ""
+    }
+  });
+
+  const vm3 = new Vue({
+    el: '#order',
+    data: {
+      oName: "",
+      oEmail: "",
+      oStreet: "",
+      oNumber: "",
+      oPayment: "",
+      oGender: "",
+      oBurger1: "",
+      oBurger2: "",
+      oBurger3: "",
+      oBurger4: "",
+      oBurger5: ""
+    },
+    methods: {
+      markDone: function info(){
+        vm3.oName = vm2.name;
+        vm3.oEmail = vm2.email;
+        vm3.oStreet = vm2.street;
+        vm3.oNumber = vm2.number;
+        vm3.oPayment = vm2.payment;
+        vm3.oGender = vm2.gender;
+
+        if (document.getElementById("maktig").checked) {
+          vm3.oBurger1 = "Mäktig Burgare";
+        } else {
+          vm3.oBurger1 = "";
+        };
+        if (document.getElementById("sjaskig").checked) {
+          vm3.oBurger2 = "Sjaskig Burgare";
+        } else {
+          vm3.oBurger2 = "";
+        };
+        if (document.getElementById("ny").checked) {
+          vm3.oBurger3 = "Nyburgare";
+        } else {
+          vm3.oBurger3 = "";
+        };
+        if (document.getElementById("liten").checked) {
+          vm3.oBurger4 = "Liten Burgare";
+        } else {
+          vm3.oBurger4 = "";
+        };
+        if (document.getElementById("toast").checked) {
+          vm3.oBurger5 = "Toast";
+        } else {
+          vm3.oBurger5 = "";
+        };
+      }
+    }
+  });
+
 
 }
 /*
